c241577e08370109ab56279de2b7ce179f2f2bb6
wangkaixin <491045545@qq.com>
2018-03-15 16:56:09 +0800

    update 03-file

--- 03-test/index.js
+++ 03-test/index.js
@@ -3,10 +3,16 @@ var btn=document.getElementById("cal");
 btn.onclick=function(){
   var width=document.getElementById("width").value;
   var height=document.getElementById("height").value;
+  if(width != null && width!="" && height!=null && height !=""){
   var pre=document.getElementById("preimeter");
   pre.value=(parseFloat(width)+parseFloat(height))*2;
   var area=document.getElementById('area');
   area.value=parseFloat(width)*parseFloat(height);
+  }
+  else{
+  var area=document.getElementById('area');
+  area.value=null;
+  }
 }
   
 
